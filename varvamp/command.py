a0ba63ebdc1456deae12ae6da4378a36e3fa5818
jonas-fuchs <jonas.michael.fuchs@googlemail.com>
2023-04-16 15:51:09 +0200

    subworkflows for the different modes

--- varvamp/command.py
+++ varvamp/command.py
@@ -138,16 +138,11 @@ def get_args(sysargs):
         return parser.parse_args(sysargs)
 
 
-def main(sysargs=sys.argv[1:]):
+def shared_workflow(args, log_file):
     """
-    main varvamp workflow
+    part of the workflow that is shared by all modes
     """
-    # start varVAMP
-    args = get_args(sysargs)
-    if not args.verbose:
-        sys.stdout = open(os.devnull, 'w')
-    start_time = time.process_time()
-    results_dir, data_dir, log_file = logging.create_dir_structure(args.input[1])
+
     logging.raise_arg_errors(args, log_file)
     logging.varvamp_progress(log_file)
     # config check
@@ -227,79 +222,224 @@ def main(sysargs=sys.argv[1:]):
         job="Filtering for primers.",
         progress_text=f"{len(left_primer_candidates)} fw and {len(right_primer_candidates)} rw potential primers"
     )
-    # the next part is specific for either SANGER/TILED mode
+
+    return alignment_cleaned, majority_consensus, ambiguous_consensus, conserved_regions, left_primer_candidates, right_primer_candidates
+
+
+def sanger_and_tiled_shared_workflow(args, left_primer_candidates, right_primer_candidates, log_file):
+    """
+    part of the workflow shared by the sanger and tiled mode
+    """
+
+    # find best primers and create primer dict
+    all_primers = primers.find_best_primers(left_primer_candidates, right_primer_candidates)
+    logging.varvamp_progress(
+        log_file,
+        progress=0.7,
+        job="Considering only high scoring primers.",
+        progress_text=f"{len(all_primers['+'])} fw and {len(all_primers['-'])} rw primers"
+    )
+    # find all possible amplicons
+    amplicons = scheme.find_amplicons(
+        all_primers,
+        args.opt_length,
+        args.max_length
+    )
+    if not amplicons:
+        logging.raise_error(
+            "no amplicons found. Increase the max amplicon length or \
+            number of ambiguous bases or lower threshold!\n",
+            log_file,
+            exit=True
+        )
+    logging.varvamp_progress(
+        log_file,
+        progress=0.8,
+        job="Finding potential amplicons.",
+        progress_text=f"{len(amplicons)} potential amplicons"
+    )
+    return all_primers, amplicons
+
+
+def sanger_workflow(args, amplicons, all_primers, log_file):
+    """
+    workflow part specific for sanger mode
+    """
+
+    amplicon_scheme = scheme.find_sanger_amplicons(amplicons, all_primers, args.report_n)
+    logging.varvamp_progress(
+        log_file,
+        progress=0.9,
+        job="Finding low scoring amplicons.",
+        progress_text=f"{len(amplicon_scheme[0])} low scoring amplicons."
+    )
+
+    return amplicon_scheme
+
+
+def tiled_workflow(args, amplicons, left_primer_candidates, right_primer_candidates, all_primers, ambiguous_consensus, log_file):
+    """
+    part of the workflow specific for the tiled mode
+    """
+
+    amplicon_graph = scheme.create_amplicon_graph(amplicons, args.overlap)
+    # search for amplicon scheme
+    coverage, amplicon_scheme = scheme.find_best_covering_scheme(
+        amplicons,
+        amplicon_graph,
+        all_primers
+    )
+    dimers_not_solved = scheme.check_and_solve_heterodimers(
+        amplicon_scheme,
+        left_primer_candidates,
+        right_primer_candidates,
+        all_primers)
+    if dimers_not_solved:
+        logging.raise_error(
+            f"varVAMP found {len(dimers_not_solved)} primer dimers without replacements. Check the dimer file and perform the PCR for incomaptible amplicons in a sperate reaction.",
+            log_file
+        )
+        reporting.write_dimers(dir, dimers_not_solved)
+    percent_coverage = round(coverage/len(ambiguous_consensus)*100, 2)
+    logging.varvamp_progress(
+        log_file,
+        progress=0.9,
+        job="Creating amplicon scheme.",
+        progress_text=f"{percent_coverage} % total coverage with {len(amplicon_scheme[0]) + len(amplicon_scheme[1])} amplicons"
+    )
+    if percent_coverage < 70:
+        logging.raise_error(
+            "coverage < 70 %. Possible solutions:\n"
+            "\t - lower threshold\n"
+            "\t - increase amplicons lengths\n"
+            "\t - increase number of ambiguous nucleotides\n"
+            "\t - relax primer settings (not recommended)\n",
+            log_file
+        )
+    return amplicon_scheme
+
+
+def qpcr_workflow(args, alignment_cleaned, ambiguous_consensus, majority_consensus, left_primer_candidates, right_primer_candidates, log_file):
+    """
+    part of the workflow specific for the tiled mode
+    """
+    # find regions for qPCR probes
+    probe_conserved_regions = conserved.find_regions(
+        ambiguous_consensus,
+        args.pn_ambig
+    )
+    if not probe_conserved_regions:
+        logging.raise_error(
+            "no conserved regions that fullfill probe criterias! lower threshold or increase number of ambiguous chars in probe\n",
+            log_file,
+            exit=True
+        )
+    # digest probe regions
+    probe_kmers = conserved.produce_kmers(
+        probe_conserved_regions,
+        majority_consensus,
+        config.QPROBE_SIZES
+    )
+    # find potential probes
+    qpcr_probes = qpcr.get_qpcr_probes(probe_kmers, ambiguous_consensus, alignment_cleaned)
+    if not qpcr_probes:
+        logging.raise_error(
+            "no qpcr probes found\n",
+            log_file,
+            exit=True
+        )
+    logging.varvamp_progress(
+        log_file,
+        progress=0.7,
+        job="Finding qPCR probes.",
+        progress_text=f"{len(qpcr_probes)} potential qPCR probes"
+    )
+    # find unique high scoring amplicons with internal probe
+    qpcr_scheme_candidates = qpcr.find_qcr_schemes(qpcr_probes, left_primer_candidates, right_primer_candidates, majority_consensus)
+    if not qpcr_scheme_candidates:
+        logging.raise_error(
+            "no qPCR scheme candidates found. lower threshold or increase number of ambiguous chars in primer and/or probe\n",
+            log_file,
+            exit=True
+        )
+    logging.varvamp_progress(
+        log_file,
+        progress=0.8,
+        job="Finding unique amplicons with probe.",
+        progress_text=f"{len(qpcr_scheme_candidates)} unique amplicons with internal probe"
+    )
+    final_schemes = qpcr.test_amplicon_deltaG(qpcr_scheme_candidates, majority_consensus, args.test_n)
+    if not final_schemes:
+        logging.raise_error(
+            "no qPCR amplicon passed the deltaG threshold\n",
+            log_file,
+            exit=True
+        )
+    logging.varvamp_progress(
+        log_file,
+        progress=0.9,
+        job="Filtering amplicons for deltaG.",
+        progress_text=f"{len(final_schemes)} non-overlapping qPCR schemes that passed deltaG cutoff"
+    )
+    reporting.varvamp_plot(
+        results_dir,
+        alignment_cleaned,
+        conserved_regions,
+        probe_conserved_regions=probe_conserved_regions
+    )
+    reporting.write_conserved_to_bed(probe_conserved_regions, data_dir, "probe")
+    reporting.write_qpcr_to_files(results_dir, final_schemes, ambiguous_consensus)
+
+    return probe_conserved_regions, final_schemes
+
+
+def main(sysargs=sys.argv[1:]):
+    """
+    main varvamp workflow
+    """
+
+    # start varVAMP
+    args = get_args(sysargs)
+    if not args.verbose:
+        sys.stdout = open(os.devnull, 'w')
+    start_time = time.process_time()
+    results_dir, data_dir, log_file = logging.create_dir_structure(args.input[1])
+
+    # mode unspecific part of the workflow
+    alignment_cleaned, majority_consensus, ambiguous_consensus, conserved_regions, left_primer_candidates, right_primer_candidates = shared_workflow(args, log_file)
+
+    # write files that are shared in all modes
+    reporting.write_conserved_to_bed(conserved_regions, data_dir)
+    reporting.write_alignment(data_dir, alignment_cleaned)
+    reporting.write_fasta(data_dir, "majority_consensus", majority_consensus)
+    reporting.write_fasta(results_dir, "ambiguous_consensus", ambiguous_consensus)
+
+    # SANGER/TILED mode
     if args.mode == "tiled" or args.mode == "sanger":
-        # find best primers and create primer dict
-        all_primers = primers.find_best_primers(left_primer_candidates, right_primer_candidates)
-        logging.varvamp_progress(
-            log_file,
-            progress=0.7,
-            job="Considering only high scoring primers.",
-            progress_text=f"{len(all_primers['+'])} fw and {len(all_primers['-'])} rw primers"
-        )
-        # find all possible amplicons
-        amplicons = scheme.find_amplicons(
-            all_primers,
-            args.opt_length,
-            args.max_length
-        )
-        if not amplicons:
-            logging.raise_error(
-                "no amplicons found. Increase the max amplicon length or \
-                number of ambiguous bases or lower threshold!\n",
-                log_file,
-                exit=True
-            )
-        logging.varvamp_progress(
-            log_file,
-            progress=0.8,
-            job="Finding potential amplicons.",
-            progress_text=f"{len(amplicons)} potential amplicons"
+        all_primers, amplicons = sanger_and_tiled_shared_workflow(
+            args,
+            left_primer_candidates,
+            right_primer_candidates,
+            log_file
         )
         if args.mode == "sanger":
-            amplicon_scheme = scheme.find_sanger_amplicons(amplicons, all_primers, args.report_n)
-            logging.varvamp_progress(
-                log_file,
-                progress=0.9,
-                job="Finding low scoring amplicons.",
-                progress_text=f"{len(amplicon_scheme[0])} low scoring amplicons."
+            amplicon_scheme = sanger_workflow(
+                args,
+                amplicons,
+                all_primers,
+                log_file
             )
         elif args.mode == "tiled":
-            amplicon_graph = scheme.create_amplicon_graph(amplicons, args.overlap)
-            # search for amplicon scheme
-            coverage, amplicon_scheme = scheme.find_best_covering_scheme(
+            amplicon_scheme = tiled_workflow(
+                args,
                 amplicons,
-                amplicon_graph,
-                all_primers
-            )
-            dimers_not_solved = scheme.check_and_solve_heterodimers(
-                amplicon_scheme,
                 left_primer_candidates,
                 right_primer_candidates,
-                all_primers)
-            if dimers_not_solved:
-                logging.raise_error(
-                    f"varVAMP found {len(dimers_not_solved)} primer dimers without replacements. Check the dimer file and perform the PCR for incomaptible amplicons in a sperate reaction.",
-                    log_file
-                )
-                reporting.write_dimers(dir, dimers_not_solved)
-            percent_coverage = round(coverage/len(ambiguous_consensus)*100, 2)
-            logging.varvamp_progress(
-                log_file,
-                progress=0.9,
-                job="Creating amplicon scheme.",
-                progress_text=f"{percent_coverage} % total coverage with {len(amplicon_scheme[0]) + len(amplicon_scheme[1])} amplicons"
+                all_primers,
+                ambiguous_consensus,
+                log_file
             )
-            if percent_coverage < 70:
-                logging.raise_error(
-                    "coverage < 70 %. Possible solutions:\n"
-                    "\t - lower threshold\n"
-                    "\t - increase amplicons lengths\n"
-                    "\t - increase number of ambiguous nucleotides\n"
-                    "\t - relax primer settings (not recommended)\n",
-                    log_file
-                )
-        # write  SANGER/TILED specific files
+        # write files
         reporting.write_all_primers(data_dir, all_primers)
         reporting.write_scheme_to_files(
             results_dir,
@@ -309,77 +449,30 @@ def main(sysargs=sys.argv[1:]):
         )
         reporting.varvamp_plot(
             results_dir,
-            args.threshold,
             alignment_cleaned,
             conserved_regions,
-            all_primers,
-            amplicon_scheme,
+            all_primers=all_primers,
+            amplicon_scheme=amplicon_scheme,
         )
-    # specific for QPCR mode
+        reporting.per_base_mismatch_plot(results_dir, amplicon_scheme, args.threshold)
+    # QPCR mode
     if args.mode == "qpcr":
-        # find regions for qPCR probes
-        probe_conserved_regions = conserved.find_regions(
+        probe_conserved_regions, final_schemes = qpcr_workflow(
+            args,
+            alignment_cleaned,
             ambiguous_consensus,
-            args.pn_ambig
-        )
-        if not probe_conserved_regions:
-            logging.raise_error(
-                "no conserved regions that fullfill probe criterias! lower threshold or increase number of ambiguous chars in probe\n",
-                log_file,
-                exit=True
-            )
-        # digest probe regions
-        probe_kmers = conserved.produce_kmers(
-            probe_conserved_regions,
             majority_consensus,
-            config.QPROBE_SIZES
+            left_primer_candidates,
+            right_primer_candidates,
+            log_file
         )
-        # find potential probes
-        qpcr_probes = qpcr.get_qpcr_probes(probe_kmers, ambiguous_consensus, alignment_cleaned)
-        if not qpcr_probes:
-            logging.raise_error(
-                "no qpcr probes found\n",
-                log_file,
-                exit=True
-            )
-        logging.varvamp_progress(
-            log_file,
-            progress=0.7,
-            job="Finding qPCR probes.",
-            progress_text=f"{len(qpcr_probes)} potential qPCR probes"
+        reporting.varvamp_plot(
+            results_dir,
+            alignment_cleaned,
+            conserved_regions,
+            probe_conserved_regions=probe_conserved_regions
         )
-        # find unique high scoring amplicons with internal probe
-        qpcr_scheme_candidates = qpcr.find_qcr_schemes(qpcr_probes, left_primer_candidates, right_primer_candidates, majority_consensus)
-        if not qpcr_scheme_candidates:
-            logging.raise_error(
-                "no qPCR scheme candidates found. lower threshold or increase number of ambiguous chars in primer and/or probe\n",
-                log_file,
-                exit=True
-            )
-        logging.varvamp_progress(
-            log_file,
-            progress=0.8,
-            job="Finding unique amplicons with probe.",
-            progress_text=f"{len(qpcr_scheme_candidates)} unique amplicons with internal probe"
-        )
-        final_schemes = qpcr.test_amplicon_deltaG(qpcr_scheme_candidates, majority_consensus, args.test_n)
-        if not final_schemes:
-            logging.raise_error(
-                "no qPCR amplicon passed the deltaG threshold\n",
-                log_file,
-                exit=True
-            )
-        logging.varvamp_progress(
-            log_file,
-            progress=0.9,
-            job="Filtering amplicons for deltaG.",
-            progress_text=f"{len(final_schemes)} non-overlapping qPCR schemes that passed deltaG cutoff"
-        )
-        reporting.write_conserved_to_bed(probe_conserved_regions, data_dir, "probe")
-        reporting.write_qpcr_to_files(results_dir, final_schemes, ambiguous_consensus)
-    # write files that are shared in all modes
-    reporting.write_conserved_to_bed(conserved_regions, data_dir)
-    reporting.write_alignment(data_dir, alignment_cleaned)
-    reporting.write_fasta(data_dir, "majority_consensus", majority_consensus)
-    reporting.write_fasta(results_dir, "ambiguous_consensus", ambiguous_consensus)
+        reporting.per_base_mismatch_plot(results_dir, amplicon_scheme, args.threshold, mode="QPCR")
+
+    # varVAMP finished
     logging.varvamp_progress(log_file, progress=1, start_time=start_time)
