6e01ef40b0a52a83e188e892b9f4f2406fbade00
Jonas Fuchs <78491186+jonas-fuchs@users.noreply.github.com>
2025-08-12 16:49:05 +0200

    Update __init__.py

--- varvamp/__init__.py
+++ varvamp/__init__.py
@@ -1,16 +1,6 @@
-from pathlib import Path
 from importlib.metadata import version, PackageNotFoundError
-import sys
 
-source_location = Path(__file__).resolve()
-pyproject = source_location.parent.parent / "pyproject.toml"
-
-if sys.version_info >= (3, 11) and pyproject.exists():
-    import tomllib
-    with open(pyproject, "rb") as f:
-        __version__ = tomllib.load(f).get("project", {}).get("version")
-else:
-    try:
-        __version__ = version("varvamp")
-    except PackageNotFoundError:
-        __version__ = "unknown"
+try:
+    __version__ = version("bamdash")
+except PackageNotFoundError:
+    __version__ = "unknown"
