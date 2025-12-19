b946dea8163f2913d6e8e23f1f4f7c4224984766
jonas <jonas.fuchs@uniklinik-freiburg.de>
2025-12-19 10:41:18 +0100

    logic update and doku

--- varvamp/scripts/param_estimation.py
+++ varvamp/scripts/param_estimation.py
@@ -59,6 +59,28 @@ def get_parameters(preprocessed_alignment, args, log_file):
     """
     give an estimate for number of ambiguous chars and/or threshold
     writes to log file
+
+    How it works:
+
+    - calculate a np array with frequencies per position for ATCG and gaps (-)
+    - now increment for different threshold starting at 0.1
+    - for each increment calculate:
+        - the lengths of conseq. nucleotides that are >= threshold
+        - skip all positions with gaps >= 1 - threshold
+        - this results in an array of stretch lengths
+        - Then check for each stretch if it could be considered as a primer region:
+            e.g. 4 ambiguous bases:
+                stretches: [1,1,1,1,6,10,5,3,1,1]
+                - first check for the current stretch if the sum of the next n stretches (with n being
+                the n ambiguous bases + 1) is larger than the minimal length of a primer
+                - this would result that the with stars marked positions would pass:
+                [1,1,1,1*,6*,10*,5,3,1,1]
+        - now calculate the distance between the current start of a stretch and the previous stop
+        - if this is larger than the minimal amplicon length - exit the optimization
+        - reset the threhold to second prior iteration to make it more robust (allow more primer regions) as
+        sometimes the optimization would fail but just 0.1
+
+    This means that manual optimization can be beneficial in some cases.
     """
     # set coverage to max
     args.threshold = 0.1
@@ -77,15 +99,16 @@ def get_parameters(preprocessed_alignment, args, log_file):
             # args.opt_length - 2 * args.overlap
             for idx, dis in enumerate(distances):
                 if sum(distances[idx:idx + 1 + args.n_ambig]) >= config.PRIMER_SIZES[1]:
-                    # the stretch start in the gap-excluded alignment is the sum of all prior distances
-                    stretch_start = sum(distances[:idx])
+                    # the stretch start in the gap-excluded alignment is the sum of all prior distances including the current
+                    # minus the distance of the current stretch
+                    stretch_start = sum(distances[:idx+1]) - distances[idx]
                     # then the distance between the prior stop and current start is calculated
                     current_dis = stretch_start - previous_stop
                     # and the max is updated if necessary
                     if max_distance_between_passing < current_dis:
                         max_distance_between_passing = current_dis
                     # update previous stop position
-                    previous_stop = stretch_start + distances[idx]
+                    previous_stop = sum(distances[:idx+1])
             # write each iteration to log
             print(round(args.threshold, 2), max_distance_between_passing, sep="\t", file=f)
             # check if the distance is acceptable
